3a006def3fd4d451648a14e8c4fc7cfb4d81a5df
Victor Chisomo Mjimapemba <vcmjimapemba@gmail.com>
2024-02-06 01:49:00 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='django-raspberrypi',
+    version='0.1',
+    packages=['django_raspberrypi'],
+    install_requires=[
+        'Django',
+        # Add other dependencies here
+    ],
+)
